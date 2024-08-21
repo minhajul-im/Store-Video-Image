9f4df90eda6a01240c80523896545bd3ef794053
minhajul <dev.minhajul@gmail.com>
2024-08-21 21:42:59 +0600

    drag and drop the image

---
+++ src/component/Extension.tsx
@@ -0,0 +1,12 @@
+const Extension = () => {
+  return (
+    <p className="mt-12 text-gray-400 text-sm text-wrap">
+      Accepted formats: JPG, JPEG, GIF, PNG, WEBP, SVG. Maximum file size:
+      <span className="px-4 mx-2 py-1 bg-gradient-to-r from-fuchsia-500 to-cyan-500 rounded-full text-TX-main">
+        3 MB
+      </span>
+    </p>
+  );
+};
+
+export default Extension;
---
+++ src/component/WithOutDrag.tsx
@@ -0,0 +1,18 @@
+import Upload from "../icons/Upload";
+
+const WithOutDrag = () => {
+  return (
+    <>
+      <Upload />
+      <header className="mt-6">
+        <span className="drag-file">Click here</span> and
+        <button className="px-2 mx-2 py-1 bg-gradient-to-r from-fuchsia-500 to-cyan-500 rounded-full file-input-button cursor-pointer">
+          select a file
+        </button>
+        from your device!
+      </header>
+    </>
+  );
+};
+
+export default WithOutDrag;
